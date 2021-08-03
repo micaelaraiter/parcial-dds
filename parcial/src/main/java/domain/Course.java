3ca58022448a060530570e0a36909c495d43d0b5
lumelamed <lumelamed@gmail.com>
2021-08-03 11:00:39 -0300

    agrego getters y setters, se me modifico el Parcial.iml y me parecio bien subirlo

--- parcial/src/main/java/domain/Course.java
+++ parcial/src/main/java/domain/Course.java
@@ -4,4 +4,20 @@ public class Course {
 
     private String name;
     private String code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
--- parcial/src/main/java/domain/Student.java
+++ parcial/src/main/java/domain/Student.java
@@ -3,9 +3,29 @@ package domain;
 public class Student {
     private Number id;
     private Course course;
-    private boolean isAproved;
+    private boolean hasPassed;
 
+    public Number getId() {
+        return id;
+    }
 
+    public void setId(Number id) {
+        this.id = id;
+    }
 
+    public Course getCourse() {
+        return course;
+    }
 
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public boolean isHasPassed() {
+        return hasPassed;
+    }
+
+    public void setHasPassed(boolean hasPassed) {
+        this.hasPassed = hasPassed;
+    }
 }
