5ceffab61474f32ee1adef7fb1476a56298fb36f
mraiter <mraiter@kovix-group.com>
2021-08-08 17:28:30 -0300

    add new tables

--- src/main/java/controller/ConnectionToDB.java
+++ src/main/java/controller/ConnectionToDB.java
@@ -17,14 +17,17 @@ public class ConnectionToDB {
             Connection connection = DriverManager.getConnection(jdbcURL, username, password);
             this.connection = connection;
             this.createUserTable();
+            this.createTeacherTable();
+            this.createCourseTable();
+            this.createStudentTable();
         } catch (Error error) {
             System.out.println(error);
         }
     }
 
     public void createUserTable() throws SQLException {
-        try{
-            String sql = "CREATE TABLE IF NOT EXISTS users (\n" +
+        try {
+            String sql = "CREATE TABLE IF NOT EXISTS user (\n" +
                     "    user_id INT AUTO_INCREMENT PRIMARY KEY,\n" +
                     "    firstName VARCHAR(255),\n" +
                     "    lastName VARCHAR(255),\n" +
@@ -34,11 +37,73 @@ public class ConnectionToDB {
                     ")  ENGINE=INNODB;";
             Statement statement = this.connection.createStatement();
             boolean rows = statement.execute(sql);
-        } catch (Error error){
+        } catch (Error error) {
             System.out.println(error);
         }
 
 
+    }
+
+    public void createTeacherTable() throws SQLException {
+        try {
+            String sql = "CREATE TABLE IF NOT EXISTS teacher (\n" +
+                    "teacher_id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+                    "CONSTRAINT FK_User FOREIGN KEY (user_id)  REFERENCES User(user_id),\n" +
+                    ")  ENGINE=INNODB;";
+            Statement statement = this.connection.createStatement();
+            boolean rows = statement.execute(sql);
+        } catch (Error error) {
+            System.out.println(error);
+        }
+    }
+
+    public void createCourseTable() throws SQLException {
+        try {
+            String sql = "CREATE TABLE IF NOT EXISTS student (\n" +
+                    "course_id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+                    "name VARCHAR(255),\n" +
+                    "code VARCHAR(255),\n" +
+                    "CONSTRAINT FK_Teacher FOREIGN KEY (teacher_id)  REFERENCES Teacher(teacher_id),\n" +
+                    ")  ENGINE=INNODB;";
+            Statement statement = this.connection.createStatement();
+            boolean rows = statement.execute(sql);
+        } catch (Error error) {
+            System.out.println(error);
+        }
+    }
+
+
+    public void createStudentTable() throws SQLException {
+        try {
+            String sql = "CREATE TABLE IF NOT EXISTS student (\n" +
+                    "student_id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+                    "CONSTRAINT FK_User FOREIGN KEY (user_id)  REFERENCES User(user_id),\n" +
+                    "CONSTRAINT FK_Course FOREIGN KEY (course_id)  REFERENCES Course(course_id),\n" +
+                    ")  ENGINE=INNODB;";
+            Statement statement = this.connection.createStatement();
+            boolean rows = statement.execute(sql);
+        } catch (Error error) {
+            System.out.println(error);
+        }
+    }
+
+    //todo:esperando correcion 
+
+    public void createHomeworkTable() throws SQLException {
+        try {
+            String sql = "CREATE TABLE IF NOT EXISTS homework (\n" +
+                    "homework_id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+                    "title VARCHAR(255),\n" +
+                    "description VARCHAR(255),\n" +
+                    "due_date DATE,\n" +
+                    "CONSTRAINT FK_User FOREIGN KEY (user_id)  REFERENCES User(user_id),\n" +
+                    "CONSTRAINT FK_Course FOREIGN KEY (course_id)  REFERENCES Course(course_id),\n" +
+                    ")  ENGINE=INNODB;";
+            Statement statement = this.connection.createStatement();
+            boolean rows = statement.execute(sql);
+        } catch (Error error) {
+            System.out.println(error);
+        }
     }
 
     public void closeDb() throws SQLException {
